0a0fb96292b4fb0bec92ba514d354b902abf44ea
Sreeraj <154326486+C-O-D-E-G-A-M-E-R@users.noreply.github.com>
2024-08-13 19:19:03 +0530

    Add files via upload

---
+++ COUNTRY INFO APP/script.js
@@ -0,0 +1,58 @@
+let searchform = document.querySelector("#search-form");
+let countryinput = document.querySelector("#country-input");
+searchform.addEventListener("submit",async(e) => {
+    e.preventDefault();
+  let countryName = countryinput.value;
+
+  try{
+  let api = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  let response=await fetch(api);
+  let data=await response.json();
+  result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(", ")}</span>
+            </div>
+        </div>
+      `;
+  }catch(error){
+    
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+  }
+});
